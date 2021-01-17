671ad8f1f28e00a3e7af50ac813c0557ddd7fe48
tutorialmulu <77432915+tutorialmulu@users.noreply.github.com>
2021-01-18 01:17:53 +0700

    Update funmenu.js

--- src/funmenu.js
+++ src/funmenu.js
@@ -1,21 +1,21 @@
 const funmenu = (prefix) => { 
 	return `
-✎═─⊱〘 𝐹𝑈𝑁 𝑀𝐸𝑁𝑈 〙⊰══
+╔══✪〘 FUN MENU 〙✪══════
 ║
-╰─⊱ *${prefix}tebakgambar*
-╰─⊱ *${prefix}caklontong*
-╰─⊱ *${prefix}family100*
-╰─⊱ *${prefix}game*
-╰─⊱ *${prefix}truth*
-╰─⊱ *${prefix}dare*
-╰─⊱ *${prefix}quotes*
-╰─⊱ *${prefix}hilih*
-╰─⊱ *${prefix}alay* [text]
-╰─⊱ *${prefix}simi* [text]
-╰─⊱ *${prefix}bucin*
-╰─⊱ *${prefix}gtts* [text]
-╰─⊱ *${prefix}tts*
+╠➥*${prefix}tebakgambar*
+╠➥*${prefix}caklontong*
+╠➥*${prefix}family100*
+╠➥*${prefix}game*
+╠➥*${prefix}truth*
+╠➥*${prefix}dare*
+╠➥*${prefix}quotes*
+╠➥*${prefix}hilih*
+╠➥*${prefix}alay* [text]
+╠➥*${prefix}simi* [text]
+╠➥*${prefix}bucin*
+╠➥*${prefix}gtts* [text]
+╠➥*${prefix}tts*
 ║
-✎═─⊱〘 𝐹𝑈𝑁 𝑀𝐸𝑁𝑈 〙⊰══`
+╔══✪〘 FUN MENU 〙✪══════
 }
 exports.funmenu = funmenu
